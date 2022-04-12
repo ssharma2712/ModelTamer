9095b2bf8243c41d64afa0a87cc853a7dbae725a
Sudip Sharma <ssharma@isrt.ac.bd>
2022-04-12 04:21:22 -0400

    Update MT_automatic.R

--- Code/MT_automatic.R
+++ Code/MT_automatic.R
@@ -14,6 +14,9 @@ MT_automatic <- function(data_path, data_type = c("DNA", "AA"), Redo = FALSE, ma
   if (!requireNamespace("stringr", quietly = TRUE))
     install.packages("stringr")
   
+  if (!requireNamespace("lubridate", quietly = TRUE))
+    install.packages("lubridate") 
+  
   ########## Library required ##########
   
   if (!library('Biostrings',logical.return = TRUE)){
@@ -22,6 +25,10 @@ MT_automatic <- function(data_path, data_type = c("DNA", "AA"), Redo = FALSE, ma
   
   if (!library('stringr',logical.return = TRUE)){
     stop("'stringr' package not found, please install it to run MT_automatic")
+  }
+
+  if (!library('lubridate',logical.return = TRUE)){
+    stop("'lubridate' package not found, please install it to run MT_automatic")
   }
   
   
@@ -46,7 +53,7 @@ MT_automatic <- function(data_path, data_type = c("DNA", "AA"), Redo = FALSE, ma
     sln1 <- sample(1:sln, expected_site, replace = F, prob = NULL)
     sln2 <- sample(sln1, sln, replace = T)
         
-    print(c("% Distinct site pattern sampled = ", paste(signif((length(unique(sln1))/num_of_dsp)*100, 4),"%", sep ="") ))
+    #print(c("% Distinct site pattern sampled = ", paste(signif((length(unique(sln1))/num_of_dsp)*100, 4),"%", sep ="") ))
     SU_dataset <- endoapply(motherfile, function(x) x[sln2])
         
     file_name <- paste(SU_name, sep = "")
@@ -54,17 +61,92 @@ MT_automatic <- function(data_path, data_type = c("DNA", "AA"), Redo = FALSE, ma
     
   }
       
+############################ ModelTamer Aggregation #####################################
+  MT_aggregator <- function(log_file_path, iqtree_file_path, data_t = c("DNA", "AA"), num_of_dsp, MT_time = NULL){
   
-  ##################################################
+  # log_file_path           : path for the log file 
+  # iqtree_file_path        : % of distinct site patterns required in a subsample
+  # data_t                  : DNA or Amino Acid (AA)
+  # num_of_dsp              : Number of distinct site patterns
   
-  f_name <- data_path                                                  # Mother MSA data path
-  motherfile <- Biostrings::readAAStringSet(f_name, format = "fasta")  # Reading the mother file
-  sln <- as.numeric(fasta.seqlengths(f_name)[1])                       # sequence length
-  a <- as.data.frame(t(as.matrix(motherfile)))                         # convert MSA into a matrix
+  #############log to Substitution matrix extraction ####################
+  log2submat <- function(iqtree_file){
+    cline1 <- which (iqtree_file == grep(pattern = "Rate matrix Q:", iqtree_file, value = TRUE))
+    iqtree_file <- iqtree_file[(cline1+2):(cline1+5)]
+    iqtree_file <- sapply(iqtree_file, function(x){unlist(strsplit(x, "\\s+"))})
+    
+    iqtree_file <- data.frame(matrix(unlist(iqtree_file), nrow=4, byrow=TRUE),stringsAsFactors=FALSE)
+    rownames(iqtree_file) <- iqtree_file[,2]
+    iqtree_file <- iqtree_file[,-c(1,2)]
+    colnames(iqtree_file) <- rownames(iqtree_file)
+    return(iqtree_file)
+  }
+  ##########################################################################
   
-  a2 <- unique(a)                                                      # Finding distinct site patterns
-  a2$ID <- as.vector(as.numeric(rownames(unique(a))))                  # Positions containing distinct site patterns
-  a2 <- merge(a, a2)                                  
+  output <- list()
+  
+  a_iqtree <- readLines(iqtree_file_path)
+  a_log    <- readLines(log_file_path)
+  
+  cline1 <- which (a_iqtree == grep(pattern = "Input data:", a_iqtree, value = TRUE))
+  cline2 <- which (a_log == grep(pattern = "Corrected Akaike Information Criterion:", a_log, value = TRUE))
+  
+  output[[1]] <- "\n"
+  output[[2]] <- "Best-fit model by Information Criteria (ModelTamer)"
+  output[[3]] <- "\n\n"
+  output[[4]] <- a_log[cline2+1]
+  output[[5]] <- "\n"
+  output[[6]] <- a_log[cline2-1]
+  output[[7]] <- "\n"
+  output[[8]] <- a_log[cline2]
+  
+  output[[9]] <- "\n\n"
+  output[[10]] <- a_iqtree[cline1]
+  output[[11]] <- "\n"
+  output[[12]] <- paste(a_iqtree[cline1+3], "(reported by IQTREE)", sep = " ")
+  output[[11]] <- "\n"
+  output[[12]] <- paste("Total distinct site patterns:  ", format(num_of_dsp, big.mark=",",scientific=FALSE), sep = "")
+  output[[13]] <- "\n"
+  output[[14]] <- paste("Distinct patterns used by ModelTamer: ",sapply(a_iqtree[cline1+4], function(x){unlist(strsplit(x, "\\s+"))})[6], sep = "")
+  output[[15]] <- "\n\n"
+  
+  cline2 <- which (a_log == grep(pattern = "NOTE: ModelFinder requires", a_log, value = TRUE))
+  a <- sapply(a_log[cline2], function(x){unlist(strsplit(x, "\\s+"))})[4:6]
+  output[[16]] <- paste("Peak memory used by ModelTamer: ",a[1], a[2], a[3], sep = " ")
+  output[[17]] <- "\n"
+  
+  if(is.null(MT_time) == TRUE){
+  cline2 <- which(a_log == grep(pattern = "CPU time for ModelFinder:", a_log, value = TRUE))
+  a <- sapply(a_log[cline2], function(x){unlist(strsplit(x, "\\s+"))})[5:7]
+  output[[18]] <- paste("CPU time for ModelTamer: ",a[1], a[2], a[3], sep = " ")
+  
+  output[[19]] <- "\n"
+  a <- sapply(a_log[cline2+1], function(x){unlist(strsplit(x, "\\s+"))})[5:7]
+  output[[20]] <- paste("Wall-clock time for ModelTamer: ",a[1], a[2], a[3], sep = " ")
+  }else{
+    formated_time <- function(x) { tolower(str_replace(str_replace(seconds_to_period(x), " ", ":"), " ", ":"))}
+    output[[18]] <- paste("CPU time for ModelTamer: ", MT_time[1], "seconds", "(", formated_time(MT_time[1]), ")" , sep = " ")
+    output[[19]] <- "\n"
+    output[[20]] <- paste("Wall-clock time for ModelTamer: ", MT_time[2], "seconds", "(", formated_time(MT_time[2]), ")" , sep = " ")
+  }
+  
+  sink("MT_output.txt")
+  cat(unlist(output))
+  sink()
+  
+}
+
+
+################################################################################
+  
+f_name <- data_path                                                  # Mother MSA data path
+motherfile <- Biostrings::readAAStringSet(f_name, format = "fasta")  # Reading the mother file
+sln <- as.numeric(fasta.seqlengths(f_name)[1])                       # sequence length
+a <- as.data.frame(t(as.matrix(motherfile)))                         # convert MSA into a matrix
+  
+a2 <- unique(a)                                                      # Finding distinct site patterns
+a2$ID <- as.vector(as.numeric(rownames(unique(a))))                  # Positions containing distinct site patterns
+a2 <- merge(a, a2)                                  
   
   distinct_positions <- a2$ID
   distinct_prob      <- prop.table(distinct_positions) 
@@ -99,130 +181,43 @@ MT_automatic <- function(data_path, data_type = c("DNA", "AA"), Redo = FALSE, ma
                     g = g_est, SU_name = SU_name1, sln = sln)
     
     if(as.character(Sys.info()[1]) == "Windows"){
-      ex_cmd <- paste("iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
+      ex_cmd <- paste("/Example/iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
       system(ex_cmd)
     }else{
-      ex_cmd <- paste("./iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
+      ex_cmd <- paste("/Example/iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
       system(ex_cmd)
     }
     
-   
-    sink("MT_output.txt", append = T)
+    MT_aggregator(paste("/Example/", SU_name1, ".log", sep = ""), paste("/Example/", SU_name1, ".iqtree", sep = ""), data_t = data_type, num_of_dsp = num_of_dsp)
     
-    a_log <- readLines(paste(SU_name1, ".log", sep = ""))
-    cline1 <- which(a_log == grep(pattern = "Best-fit model:", a_log, value = TRUE))
-    a_log <- a_log[cline1]
-    final_model <- sapply(a_log, function(x){unlist(strsplit(x, "\\s+"))})[,][3]
-    print(paste("Final Model (BIC):", final_model, sep = " "))
-    sink()
     
   }else{
+    MT_time <- c(0,0)
     g1 <- (1:max.iter)*g_est
     g1 <- c(g1[g1<63], 100)
     g1 <- g1[1:max.iter]
     
     for(i in 1:length(g1)){
       SU_name1 <- paste(SU_name_base, "_g_", g1[i], ".fasta", sep = "")
-      MT_sampler_auto(motherfile, num_of_dsp = num_of_dsp, distinct_positions = distinct_positions,
-                      g = g1[i], SU_name = SU_name1, sln = sln)
+      MT_sampler_auto(motherfile, num_of_dsp = num_of_dsp, distinct_positions = distinct_positions, g = g1[i], SU_name = SU_name1, sln = sln)
       
       if(as.character(Sys.info()[1]) == "Windows"){
-        ex_cmd <- paste("iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
+        ex_cmd <- paste("/Example/iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
         system(ex_cmd)
       }else{
-        ex_cmd <- paste("./iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
+        ex_cmd <- paste("/Example/iqtree -s", SU_name1, "-m MF -nt 1 -quiet",  sep = " " )
         system(ex_cmd)
       }  # else
-      
-      sink("MT_output.txt", append = T)
-      print(c("Iteration:", i))
-      cat("\n\n")
-      a_log <- readLines(paste(SU_name1, ".log", sep = ""))
-      cline1 <- which (a_log == grep(pattern = "Best-fit model:", a_log, value = TRUE))
-      a_log <- a_log[cline1]
-      final_model <- sapply(a_log, function(x){unlist(strsplit(x, "\\s+"))})[,][3]
-      print(paste("Final Model (BIC) in iteration", i, ":", final_model, sep = " "))
-      cat("\n\n\n")
-      sink()
-      
+    temp <- readLines(paste("/Example/", SU_name1, ".log", sep = ""))
+    cline <- which (temp == grep(pattern = "CPU time for ModelFinder:", temp, value = TRUE))
+    ct <- as.numeric(sapply(temp[cline], function(x){unlist(strsplit(x, "\\s+"))})[5])
+    wt <- as.numeric(sapply(temp[cline+1], function(x){unlist(strsplit(x, "\\s+"))})[5])
+    MT_time <- MT_time + c(ct, wt)
+
     }    # for loop
+  MT_aggregator(paste("/Example/", SU_name1, ".log", sep = ""), paste("/Example/", SU_name1, ".iqtree", sep = ""), data_t = data_type, num_of_dsp = num_of_dsp, MT_time = MT_time)
+
   }      # else
 
-  
 
 }
-
-
-###############  MT_aggregator  #######################
-
-MT_aggregator <- function(log_file_path, iqtree_file_path, data_t = c("DNA", "AA")){
-  
-  # log_file_path           : path for the log file 
-  # iqtree_file_path        : % of distinct site patterns required in a subsample
-  # data_type               : DNA or Amino Acid (AA)
-  
-  #############log to Substitution matrix extraction ####################
-  log2submat <- function(iqtree_file){
-    cline1 <- which (iqtree_file == grep(pattern = "Rate matrix Q:", iqtree_file, value = TRUE))
-    iqtree_file <- iqtree_file[(cline1+2):(cline1+5)]
-    iqtree_file <- sapply(iqtree_file, function(x){unlist(strsplit(x, "\\s+"))})
-    
-    iqtree_file <- data.frame(matrix(unlist(iqtree_file), nrow=4, byrow=TRUE),stringsAsFactors=FALSE)
-    rownames(iqtree_file) <- iqtree_file[,2]
-    iqtree_file <- iqtree_file[,-c(1,2)]
-    colnames(iqtree_file) <- rownames(iqtree_file)
-    return(iqtree_file)
-  }
-  ##########################################################################
-  
-  output <- list()
-  
-  a_iqtree <- readLines(iqtree_file_path)
-  a_log    <- readLines(log_file_path)
-  
-  
-  cline2 <- which (a_log == grep(pattern = "Corrected Akaike Information Criterion:", a_log, value = TRUE))
-  
-  
-  output[[1]] <- a_log[cline2+2]
-  
-  output[[2]] <- a_log[cline2-1]
-  output[[3]] <- a_log[cline2]
-  output[[4]] <- a_log[cline2+1]
-  
-  cline1 <- which (a_iqtree == grep(pattern = "Input data:", a_iqtree, value = TRUE))
-  output[[5]] <- a_iqtree[cline1]
-  output[[6]] <- a_iqtree[cline1+1]
-  output[[7]] <- a_iqtree[cline1+2]
-  output[[8]] <- a_iqtree[cline1+3]
-  output[[9]] <- a_iqtree[cline1+4]
-  
-  
-  cline2 <- which (a_log == grep(pattern = "NOTE: ModelFinder requires", a_log, value = TRUE))
-  output[[10]] <- a_log[cline2]
-  
-  cline2 <- which (a_log == grep(pattern = "CPU time for ModelFinder:", a_log, value = TRUE))
-  output[[11]] <- a_log[cline2]
-  output[[12]] <- a_log[cline2+1]
-  
-  sink("MT_output.txt")
-  print(unlist(output))
-  sink()
-  
-  if(data_t == "DNA"){
-    
-    submat <- log2submat(a_iqtree)
-    submat <- sapply(submat, as.numeric)
-    rownames(submat) <- colnames(submat)
-    output[[13]] <- "Substitution Matrix (Q)"
-    sink("MT_output.txt", append = T)
-    cat("\n\n")
-    print(unlist(output[[13]]))
-    print(submat)
-    sink()
-  }else{
-    output <- output
-  }
-  
-}
-
